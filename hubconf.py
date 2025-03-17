7f661687fe39af4fa7ec497de68c62580bcf1ccc
CM Geldenhuys <4649249+CMGeldenhuys@users.noreply.github.com>
2025-03-17 09:48:26 +0200

    feat(hub): add remaining models

--- hubconf.py
+++ hubconf.py
@@ -40,6 +40,18 @@ def aves_core(**kwargs):
     return AvesModel.from_hub(_AVES_CORE)
 
 
+def aves_bio(**kwargs):
+    return AvesModel.from_hub(_AVES_BIO)
+
+
+def aves_nonbio(**kwargs):
+    return AvesModel.from_hub(_AVES_NONBIO)
+
+
+def aves_all(**kwargs):
+    return AvesModel.from_hub(_AVES_ALL)
+
+
 # Heavily based on https://github.com/pytorch/pytorch/blob/1eba9b3aa3c43f86f4a2c807ac8e12c4a7767340/torch/hub.py#L803
 # Infact, not sure why this isn't already a utility function?..
 def _fetch_aux_file_from_url(
